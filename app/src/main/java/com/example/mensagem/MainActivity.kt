44c7b6b5ea07f579cdd773a0ad76a13f3c101ea9
Lameiras <afplameiras@gmail.com>
2021-04-22 16:29:01 +0100

    Mensagem de texto

---
+++ app/src/main/java/com/example/mensagem/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.mensagem
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun enviaMensagem(View: View){
+        val editTextMensagem = findViewById<EditText>(R.id.editTextMensagem)
+        val mensagem = editTextMensagem.text.toString()
+
+        val intent = Intent(this, MostraMensagemActivity::class.java)
+        intent.putExtra("MENSAGEM", mensagem)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/mensagem/MostraMensagemActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mensagem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MostraMensagemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mostra_mensagem)
+
+        val mensagem = intent.getStringExtra("MENSAGEM")
+
+        val textViewMensagem = findViewById<TextView>(R.id.textViewMensagem)
+        textViewMensagem.text = mensagem
+    }
+}
